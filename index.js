6ebce6054ff1500806cff3c13567254d1d37d311
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-17 07:49:27 +0600

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 // app object - module staffolding
 const app = {};
@@ -26,7 +27,16 @@ app.createServer = () => {
 
 // handle reaquest and response
 app.handleReqRes = (req, res) => {
-  // handle request
+  // request handling
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  // get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
   // handle response
   res.end("hello programmers, good morning");
 };
